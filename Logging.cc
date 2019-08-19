716f804ac4b334cbf1ad7123f011f9360cf00838
seasona <396438446@qq.com>
2019-08-19 15:01:23 +0800

    add LogLevel

--- Logging.cc
+++ Logging.cc
@@ -7,20 +7,27 @@ namespace JLog {
 
 static pthread_once_t once_control = PTHREAD_ONCE_INIT;
 static AsyncLogging* async_logger;
+std::string Logger::log_filename_("JLog");
+Logger::LogLevel Logger::level_(Logger::INFO);
 
-std::string Logger::log_filename_ = "JLog.log";
+const char* log_level_name[Logger::NUM_LOG_LEVELS] = {
+    "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL",
+};
 
 void once_init() {
     async_logger = new AsyncLogging(Logger::getLogFileName(), roll_size);
 }
 
+// 异步写入数据，确保只有一个AsyncLogging单例类
 void output(const char* msg, int len) {
     pthread_once(&once_control, once_init);
     async_logger->append(msg, len);
 }
 
-Logger::Impl::Impl(const char* filename, int line)
-    : line_(line), basename_(filename) {
+Logger::Impl::Impl(const char* filename, int line, LogLevel level,
+                   const char* func)
+    : line_(line), basename_(filename), level_(level), func_(func) {
+    stream_ << "[" << log_level_name[level_] << "] ";
     formatTime();
 }
 
@@ -32,17 +39,29 @@ void Logger::Impl::formatTime() {
     time = tv.tv_sec;
     struct tm* p_time = localtime(&time);
     strftime(str_t, 26, "%Y-%m-%d %H:%M:%S", p_time);
-    stream_ << str_t<<"--( ";
+    stream_ << str_t << "-( ";
 }
 
-Logger::Logger(const char* filename, int line) : impl_(filename, line) {}
+Logger::Logger(const char* filename, int line)
+    : impl_(filename, line, INFO, NULL) {}
+
+Logger::Logger(const char* filename, int line, LogLevel level)
+    : impl_(filename, line, level, NULL) {}
+
+Logger::Logger(const char* filename, int line, LogLevel level, const char* func)
+    : impl_(filename, line, level, func) {}
 
 Logger::~Logger() {
-    // 写入格式为时间--源文件:行号\n正文
-    // 写入源文件：行号
-    impl_.stream_ << " )--" << impl_.basename_ << ":" << impl_.line_ << "\n";
+    // 写入格式为：
+    // [DEBUG] 2019-08-19 14:58:29-( 0 )-/home/jie/JLog/tests/Logging_test.cc:25:type_test()
+    impl_.stream_ << " )-" << impl_.basename_ << ":" << impl_.line_;
+    if (impl_.func_) {
+        impl_.stream_ << ":" << impl_.func_ << "()\n";
+    } else {
+        impl_.stream_ << "\n";
+    }
     const LogStream::Buffer& buf(this->stream().buffer());
     output(buf.data(), buf.length());
 }
 
-}
+}  // namespace JLog
--- tests/Logging_test.cc
+++ tests/Logging_test.cc
@@ -14,7 +14,7 @@
 
 void threadFunc() {
     for (int i = 0; i < 100000; ++i) {
-        LOG << i;
+        LOG_INFO << i;
         usleep(100);
     }
 }
@@ -22,19 +22,19 @@ void threadFunc() {
 void type_test() {
     // 13 lines
     std::cout << "----------type test-----------" << std::endl;
-    LOG << 0;
-    LOG << 123456789;
-    LOG << 1.0f;
-    LOG << 3.1415926;
-    LOG << (short)1;
-    LOG << (long long)1;
-    LOG << (unsigned int)1;
-    LOG << (unsigned long)1;
-    LOG << (long double)1.6555556;
-    LOG << (unsigned long long)1;
-    LOG << 'c';
-    LOG << "abcdefg";
-    LOG << std::string("This is a string");
+    LOG_DEBUG << 0;
+    LOG_TRACE << 123456789;
+    LOG_INFO << 1.0f;
+    LOG_INFO << 3.1415926;
+    LOG_WARN << (short)1;
+    LOG_WARN << (long long)1;
+    LOG_ERROR << (unsigned int)1;
+    LOG_FATAL << (unsigned long)1;
+    LOG_INFO << (long double)1.6555556;
+    LOG_INFO << (unsigned long long)1;
+    LOG_ERROR << 'c';
+    LOG_INFO << "abcdefg";
+    LOG_INFO << std::string("This is a string");
 }
 
 void stressing_single_thread() {
@@ -42,7 +42,7 @@ void stressing_single_thread() {
     std::cout << "----------stressing test single thread-----------"
               << std::endl;
     for (int i = 0; i < 100000; ++i) {
-        LOG << i;
+        LOG_INFO << i;
         usleep(100);
     }
 }
@@ -65,10 +65,12 @@ void stressing_multi_threads(int threadNum = 4) {
 void other() {
     // 1 line
     std::cout << "----------other test-----------" << std::endl;
-    LOG << "fddsa" << 'c' << 0 << 3.666 << std::string("This is a string");
+    LOG_INFO << "fddsa" << 'c' << 0 << 3.666 << std::string("This is a string");
 }
 
 int main() {
+    JLog::Logger::setLogFileName("test");
+    JLog::Logger::setLogLevel(JLog::Logger::TRACE);
     // 共500014行
     type_test();
     sleep(3);
